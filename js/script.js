b59b60c4e9b1f45ba2973c9b88c068724af31a7b
Mary-Keje <mary@wiseki.com>
2025-02-07 17:28:07 +0100

    made adjustment

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const allLinks = document.querySelector("a:link");
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-    console.log(href);
+const aLinks = document.querySelector("a:link");
+// aLinks.forEach(function (link) {
+  // link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    // const href = link.getAttribute("href");
+    // console.log(href);
 
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
+    // if (href !== "#" && href.startsWith("#")) {
+    //   const sectionEl = document.querySelector(href);
+    //   sectionEl.scrollIntoView({ behavior: "smooth" });
+    // }
+  // });
+// });
 
 // Sticky Navigation
-const sectionHeroEl = document.querySelector(".section-hero");
+// const sectionHeroEl = document.querySelector(".section-hero");
