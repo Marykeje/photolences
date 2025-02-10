e95a54673fd786b59a645efbfaf0ecd4df36f3c2
Mary-Keje <mary@wiseki.com>
2025-02-10 16:47:12 +0100

    made adjustment

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-const aLinks = document.querySelector("a:link");
+// const aLinks = document.querySelector("a:link");
 // aLinks.forEach(function (link) {
-  // link.addEventListener("click", function (e) {
-    // e.preventDefault();
-    // const href = link.getAttribute("href");
-    // console.log(href);
+// link.addEventListener("click", function (e) {
+// e.preventDefault();
+// const href = link.getAttribute("href");
+// console.log(href);
 
-    // if (href !== "#" && href.startsWith("#")) {
-    //   const sectionEl = document.querySelector(href);
-    //   sectionEl.scrollIntoView({ behavior: "smooth" });
-    // }
-  // });
+// if (href !== "#" && href.startsWith("#")) {
+//   const sectionEl = document.querySelector(href);
+//   sectionEl.scrollIntoView({ behavior: "smooth" });
+// }
+// });
 // });
 
 // Sticky Navigation
 // const sectionHeroEl = document.querySelector(".section-hero");
+
+const navLinks = document.querySelector(".header__nav-list");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+  });
+});
+
+// function navLink() {
+//   // console.log("hey")
+//   const aLinks = document.getElementByClass("header__nav-list");
+//   if (aLinks.style.display === "block") {
+//     aLinks.style.display = "none";
+//   } else {
+//     aLinks.style.display = "block";
+//   }
+// }
