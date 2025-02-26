3389f7276579387086123090cb20a3c8c9752e9b
Mary-Keje <mary@wiseki.com>
2025-02-26 08:58:24 +0100

    made adjustment on vision statement

--- js/script.js
+++ js/script.js
@@ -5,23 +5,6 @@
 // const href = link.getAttribute("href");
 // console.log(href);
 
-// if (href !== "#" && href.startsWith("#")) {
-//   const sectionEl = document.querySelector(href);
-//   sectionEl.scrollIntoView({ behavior: "smooth" });
-// }
-// });
-// });
-
-// Sticky Navigation
-// const sectionHeroEl = document.querySelector(".section-hero");
-
-// const navMenuEl = document.querySelector("nav-mobile");
-// const headerEl = document.querySelector("header");
-// // for click event
-// navMenuEl.addEventListerner("click", function () {
-//   headerEl.classList.toggle(".nav-open");
-//  });
-
 const navToggle = document.querySelector(".header__nav-btn");
 const navList = document.querySelector(".header__nav-list");
 
